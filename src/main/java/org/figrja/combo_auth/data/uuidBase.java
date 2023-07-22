b832deb482ef68463ea1a6fb8d0b93373dcfcd9b
FIGRJA <65646747+FIGRJA@users.noreply.github.com>
2023-07-22 14:59:53 +0300

    Add files via upload

---
+++ src/main/java/org/figrja/combo_auth/data/uuidBase.java
@@ -0,0 +1,14 @@
+package org.figrja.combo_auth.data;
+
+import com.google.common.collect.Multimap;
+import com.google.common.hash.BloomFilter;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+public class uuidBase {
+
+    public static HashMap<UUID,UUID[]> baseUUID = new HashMap();
+
+
+}
