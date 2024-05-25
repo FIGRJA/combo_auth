7d7e5cf18e4b06e5fbd0b8ed17e654f6e35c3502
FIGRJA <b-stepan.007@mail.ru>
2024-05-25 18:25:56 +0300

    change adding property logic

--- src/main/java/org/figrja/combo_auth/config/AuthSchemaList.java
+++ src/main/java/org/figrja/combo_auth/config/AuthSchemaList.java
@@ -20,7 +20,7 @@ public class AuthSchemaList {
     public PropertyMap getProperty(){
         PropertyMap map = new PropertyMap();
         for (Property p : AddProperty){
-            map.put(p.name(),p);
+            map.put(p.getName(),p);
         }
         return map;
     }
